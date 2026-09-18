85011d036f293e9ebc4bc262e99db8dcffea7f01
KshitizMainaly <kshitizmainaly3334@gmail.com>
2026-09-18 11:12:43 +0545

    fix: use valid NotificationHandling variant 'NeverShow'

--- wezterm.lua
+++ wezterm.lua
@@ -58,7 +58,7 @@ config.enable_scroll_bar = false
 config.check_for_updates = false
 config.status_update_interval = 60000   -- status bar callback fires once/min instead of default 1s
 config.use_resize_increments = true     -- prevent Neovim from aggressively redrawing on smooth window resizes
-config.notification_handling = "None"   -- completely disable all desktop notifications directly in WezTerm
+config.notification_handling = "NeverShow"   -- completely disable all desktop notifications directly in WezTerm
 
 -- Maximize throughput
 config.mux_output_parser_coalesce_delay_ms = 0
